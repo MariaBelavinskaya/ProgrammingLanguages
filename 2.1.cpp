805b660d8ce1a9c27d6a9e0b426f767827b91c80
MariaBelavinskaya <113427614+MariaBelavinskaya@users.noreply.github.com>
2022-10-13 17:36:05 +0300

    Update 2.1.cpp

--- 2.1.cpp
+++ 2.1.cpp
@@ -1,28 +1,61 @@
-﻿#define _USE_MATH_DEFINES // for C++
+#define _USE_MATH_DEFINES // for C++
 #include <cmath>
 #include<iostream>
 using namespace std;
+
 /**
 *\brief Функция, рассчитывающая площадь круга
 *\param r - радиус круга
 **/
-double squadCirc(double const int r);
+
+double getVariable(const string& message);
+
+/**
+*\brief Функция, суммирующая два числа.
+*\param a Первое число.
+*\param b Второе число.
+*\return Значение функции.
+*/
+
+double squadCirc(const int r);
+
 /**
 *\brief Функция, рассчитывающая площадь трапеции
 *\ param а - певрое основание
 *\ param b - второе основание
 *\ param h - высота
 **/
-double squadTrap(double const int a, double const int b, double const int h);
+
+double squadTrap(const int a, const int b, const int h);
+
 /**
 * \brief
 * \точка входа в программу
 * \return 0 в случае успеха
 */
-int main() 
+
+enum class userInput
 {
+	squadCirc,
+	squadTrap
+};
+
+int main()
+{
+	double a = getVariable("Введите значение перменной a: ");
+	double b = getVariable("Введите значение перменной b: ");
+	double h = getVariable("Введите значение перменной h: ");
+	double r = getVariable("Введите значение перменной r: ");
+	cout << "Введите число, соответствующее заданию:\n"
+		<< static_cast<int>(userInput::squadCirc) << " - Вычисляет площадь круга.\n"
+		<< static_cast<int>(userInput::squadTrap) << " - Вычисляет площадь трапеции.\n";
+	
+	int input = 0;
+	cin >> input;
+	const auto choice = static_cast<userInput>(input);
+	cout << "\n";
+
 	setlocale(LC_ALL, "Rus");
-	int a, b, h, r;
 	cout << "Введите  основание трапеции\n ";
 	cin >> a;
 	cout << "Введите второе основание трапеции\n ";
@@ -35,15 +68,19 @@ int main()
 	cout << "Площадь круга равна " << squadCirc(r);
 	return 0;
 }
-
-double squadCirc(double const int r) 
+double getVariable(const string& message)
 {
+    cout << message;
+    double variable = 0.0;
+    cin >> variable;
+    return variable;
+}
+
+double squadCirc(const int r) {
 	double sqd = (M_PI) * pow(r,2);
 	return sqd;
 }
-
-double squadTrap(double const int a, double const int b, double const int h) 
-{
+double squadTrap(const int a, const int b, const int h) {
 	double sqd = (a + b)/ 2 * h;
 	return sqd;
 }
